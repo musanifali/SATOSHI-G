5dd813829252937d5476aba88e0dcdab4a0370be
musanifali <musanifali300@gmail.com>
2025-09-22 03:26:16 -0700

    chore: comment out MetricsSection in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       
       {/* 🎯 HERO & TRUST */}
       <HeroSection />
-      <MetricsSection />
+      {/* <MetricsSection /> */}
       
       {/* 🏢 COMPANY & SERVICES */}
       <div id="about">
